d19edd74722a78cdb268304caa2dc649197385d2
Wesley <wesleyadwebster@gmail.com>
2023-09-15 20:37:09 -0500

    playing around getting used to Vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  content be here instead
+  <CharacterCreationForm />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import CharacterCreationForm from './components/CharacterCreationForm/CharacterCreationForm.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CharacterCreationForm
   }
 });
 </script>
 
 <style lang="scss">
+@import '../variables.scss';
+
 #app {
+  background-color: $appBackgroundColor;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/CharacterCreationForm/CharacterCreationForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h3>Character Creation Screen</h3>
+    <form>
+      <label>Full name</label>
+      <input placeholder="Full Name" type="text" />
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CharacterCreationForm',
+  components: {
+  }
+});
+</script>
